c5b8d6ae19834f3e65c4db1c446f137783dd59da
Rodrigo R. Almeida <rodrigo.roberto.almeida@gmail.com>
2022-04-22 15:18:01 -0300

    fixed cloudnary images

---
+++ config/env/production/plugins.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'wysiwyg': {
+    enabled: true,
+    resolve: './src/plugins/wysiwyg'
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {
+          upload: {
+            folder: env("CLOUDINARY_FOLDER"),
+          }
+        },
+        delete: {},
+      },
+    },
+  },
+}
